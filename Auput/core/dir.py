c7342ccf6458c72be2317a3e43b620d1059b0c93
Putra <yogaaasp73@gmail.com>
2025-11-15 07:45:06 +0700

    -+

--- Auput/core/dir.py
+++ Auput/core/dir.py
@@ -1,24 +1,24 @@
+import logging
 import os
 import sys
 from os import listdir, mkdir
 
-from ..logging import LOGGER
-
-
 def dirr():
-    if "assets" not in listdir():
-        LOGGER(__name__).warning(
-            f"Assets Folder not Found. Please clone repository again."
-        )
-        sys.exit()
     for file in os.listdir():
         if file.endswith(".jpg"):
             os.remove(file)
-    for file in os.listdir():
-        if file.endswith(".jpeg"):
+        elif file.endswith(".jpeg"):
             os.remove(file)
-    if "downloads" not in listdir():
-        mkdir("downloads")
-    if "cache" not in listdir():
-        mkdir("cache")
-    LOGGER(__name__).info("Directories Updated.")
+        elif file.endswith(".png"):
+            os.remove(file)
+
+    if "downloads" not in os.listdir():
+        os.mkdir("downloads")
+    if "cache" not in os.listdir():
+        os.mkdir("cache")
+
+    logging.info("Directories Updated.")
+
+
+if __name__ == "__main__":
+    dirr()
