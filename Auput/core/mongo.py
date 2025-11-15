50a25593b8515ae647477008acb2547350c8b184
Putra <yogaaasp73@gmail.com>
2025-11-15 07:48:55 +0700

    -+

--- Auput/core/mongo.py
+++ Auput/core/mongo.py
@@ -1,34 +1,17 @@
 from motor.motor_asyncio import AsyncIOMotorClient as _mongo_client_
 from pymongo import MongoClient
-from pyrogram import Client
 
 import config
 
 from ..logging import LOGGER
 
-TEMP_MONGODB = "mongodb+srv://userbot:userbot@userbot.nrzfzdf.mongodb.net/?retryWrites=true&w=majority"
-
-
-if config.MONGO_DB_URI is None:
-    LOGGER(__name__).warning(
-        "No MONGO DB URL found.. Your Bot will work on Nezuko Database"
-    )
-    temp_client = Client(
-        "Auput",
-        bot_token=config.BOT_TOKEN,
-        api_id=config.API_ID,
-        api_hash=config.API_HASH,
-    )
-    temp_client.start()
-    info = temp_client.get_me()
-    username = info.username
-    temp_client.stop()
-    _mongo_async_ = _mongo_client_(TEMP_MONGODB)
-    _mongo_sync_ = MongoClient(TEMP_MONGODB)
-    mongodb = _mongo_async_[username]
-    pymongodb = _mongo_sync_[username]
-else:
+LOGGER(__name__).info("⏳ Establishing a secure link to your MongoDB database...")
+try:
     _mongo_async_ = _mongo_client_(config.MONGO_DB_URI)
     _mongo_sync_ = MongoClient(config.MONGO_DB_URI)
     mongodb = _mongo_async_.Auput
     pymongodb = _mongo_sync_.Auput
+    LOGGER(__name__).info("✅ Successfully connected to MongoDB. All systems are ready!")
+except:
+    LOGGER(__name__).error("❌ MongoDB connection failed!")
+    exit()
